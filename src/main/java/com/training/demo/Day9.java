ad81d4e8d97d82d0615c8ab1bc36f505b4edf9e1
MatthewYao2000 <matthew.yao.2000@gmail.com>
2024-02-23 10:58:26 -0600

    day9

---
+++ src/main/java/com/training/demo/Day9.java
@@ -0,0 +1,216 @@
+package com.training.demo;
+/*
+
+   Day 1:
+    what is oop
+        what is object? -> state and behaviors
+    poly -> overloading and overriding
+    encapsulation -> hiding information about your class
+    abstraction
+        abstract vs interface
+    Inheritance
+
+    top interview question:
+    1: abstract vs interface,
+    2: what is oop
+    3:what are diff between == and equals()
+    4: string vs string builder vs string buffer
+    5:dose java use pass-by-value or pass-by-reference?
+    6: static class(method) vs non-static class(method)
+    7:checked exception and uncheck exception
+    8:exception vs error
+    9:final vs finally vs finalize
+    10: what is an immutable class
+    11: hashmap before java 8 vs hashmap after java 8
+
+
+    primitive type ->
+    wrapper type ->
+
+
+    string vs string builder vs string buffer
+    access modifiers
+        public
+        protected
+        default
+        private
+
+    this keyword
+    used to reference current object, as parameter, constructor
+    supper keyword
+        call parent's constructor
+        call parent's methods
+        pointing to parent class
+
+    shallow copy vs deep copy ->
+
+    dose java use pass-by-value or pass-by-reference?
+
+
+    exceptions
+    checked exception and uncheck exception
+
+    exception vs error
+
+    generic:
+        T,E,K,N,V
+
+
+        Day 2
+        top question:
+            array vs arraylist
+            arraylist vs linkedlist
+            TC: add, get remove in array list
+            HashMap vs concurrent hashmap
+            how does hashmap work?
+            how does hashing in hashmap work
+
+
+
+
+        linkedlist:
+            TC for add(), get, remove(index) add(index, element)
+
+         deque: FILO and FIFO
+
+         Queue -> array
+                max heap and heap
+
+          HashMap
+
+            hashset
+             hashset vs tree set
+             O(1)           O(LOGN)
+             allowed null, does not null
+
+
+
+      Day 3
+      Top questions:
+      what is functional interface
+      can you give some examples about functional interface-> runnable, callable, comparator, cosumer, function, supplier
+      what is lambda expression
+      how do you use lambda expression
+      what is stream api
+            intermediate functions-> filter, map, flatmap, sorted...
+            terminal function-> foreach, reduce, min, max, count, anymacth, allmacth, nonematch
+      collections vs stream api
+      optional
+        what is optional
+        why do you use optional
+        what is optional.ofnull
+        completable future vs future
+        what is method reference
+        how do you use method reference
+
+
+
+
+
+        runAsync and supplyAsync
+
+
+        Day 4
+                Top questions:
+                 what is CAS: compare and set
+                 the process of CAS
+                 what is concurrethashmap
+                 concurrethashmap in java 7 vs java 8
+                 what is blocking queue
+                 what is fixed thread pool
+                 what is fork join pool
+                 executor vs executor service vs executors
+
+         Day5
+            Top questions:
+                what is JVM
+                what are components in JVM
+                stack vs heap in JVM
+                what is stack
+                what is heap
+                what are components in heap
+                what is young gene
+                what is old gene
+                what is gc roots
+                what are GC algorithm you know
+                    normal deletion
+                    normal deletion-compacting
+                    CMS
+                    G1
+                does gc always called with system.gc() -> no
+                thread states
+                    new, ruannable, waiting, terminated
+
+                sleep vs wait()
+                what  is volatile
+                what is synchronized
+                pessimistic locker vs optimistic locker
+
+
+
+
+                how do you create thread
+
+        Day 6
+        top questions:
+            what is tcp, upd, dns, http and http vs https
+            http: methods: get, post, put, delete...
+                  status code, 1XX, 2XX,3XX,4XX,5XX
+             what is three way handshake
+             what are design pattern you know
+                singleton pattern
+                    eager initialization
+                    static block initialization
+                    lazy initialization vs eager initialization
+                    thread safe
+                     bill pugh
+              prototype pattern
+              Factory
+        Day 7
+        what is spring mvc
+        Model, view, controller
+        what is rest controller
+        rest controller vs controller
+        what is requestBody, requestParam, pathvariable annotation
+        what is global exception
+        what is controlleradvice exception
+        what is exceptionhandler
+        what is spring ioc
+        what is DI
+        what are bean life cycles
+        bean scope
+            singleton, prototype, request, session..
+
+
+        day 8
+            JPA
+            TOP question
+            @service vs controller vs repository vs component annotation
+            what spring aop
+            join point, point cut
+            @before, @after, afterReturn..
+            what is spring validations
+            what are validation annotation you know
+            what TDD process
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+
+public class Day9 {
+
+
+
+}
